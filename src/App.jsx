9f648b1faf4acfdb748c2633ac0bebeaf35ed798
Maryia <97767940+Masyak95@users.noreply.github.com>
2024-09-05 18:28:51 +0200

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import {createRoot} from 'react-dom';
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Baxik",
-      breed: "British Shorthair",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Hamster",
-      name: "Cookie",
-      breed: "Roborovski hamster",
-    }),
-  ]);
+  return <div>
+    <h1>Adopt me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havenese"/>
+  </div>;
 };
 
 const container = document.getElementById("root");
